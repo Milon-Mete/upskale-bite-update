cb3306ea09f83daa70527b6038a16bc4b3803df0
Milon Mete <milonmete07@gmail.com>
2026-07-12 22:02:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://upskale-demo.netlify.app',
+  'https://upskal.netlify.app',
   'https://upskale.co'
 ];
 
